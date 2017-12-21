781c4f01c8e5d0daccab3bc0cd92da460dcf5cd6
kszajga <csygusz@gmail.com>
2017-12-21 21:59:12 +0100

    Initial commit

---
+++ app/src/main/java/regkalk2/kszajgapp/hu/regkalk/RegKalkMain.java
@@ -0,0 +1,13 @@
+package regkalk2.kszajgapp.hu.regkalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegKalkMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_kalk_main);
+    }
+}
