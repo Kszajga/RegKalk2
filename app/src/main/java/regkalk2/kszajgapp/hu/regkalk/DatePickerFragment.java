40542ec117922024786b81a30375cf1f4808c33f
kszajga <csygusz@gmail.com>
2017-12-26 21:08:42 +0100

    DatePicker

---
+++ app/src/main/java/regkalk2/kszajgapp/hu/regkalk/DatePickerFragment.java
@@ -0,0 +1,41 @@
+package regkalk2.kszajgapp.hu.regkalk;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Csiga on 2017. 12. 26..
+ */
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Button btn_ElsoForgDatum = (Button) getActivity().findViewById(R.id.btn_ElsoForgDatum);
+        // Do something with the date chosen by the user
+        Locale current = getResources().getConfiguration().locale;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy. MMMM dd.", current);
+        btn_ElsoForgDatum.setText(dateFormat.format(new Date(year-1900, month, day)));
+    }
+}
--- app/src/main/java/regkalk2/kszajgapp/hu/regkalk/RegKalkMain.java
+++ app/src/main/java/regkalk2/kszajgapp/hu/regkalk/RegKalkMain.java
@@ -1,13 +1,42 @@
 package regkalk2.kszajgapp.hu.regkalk;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class RegKalkMain extends AppCompatActivity {
+
+    private Spinner spn_KornyOszt, spn_Uzemanyag, spn_Loketterfogat, spn_Vizsga;
+    private EditText et_Teljesitmeny;
+    private Button btn_Szamol, btn_ElsoForgDatum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_kalk_main);
+
+        initUI();
+    }
+
+    public void initUI() {
+        spn_KornyOszt = (Spinner) findViewById(R.id.spn_KornyOszt);
+        spn_Uzemanyag = (Spinner) findViewById(R.id.spn_Uzemanyag);
+        spn_Loketterfogat = (Spinner) findViewById(R.id.spn_Loketterfogat);
+        spn_Vizsga = (Spinner) findViewById(R.id.spn_Vizsga);
+        et_Teljesitmeny = (EditText) findViewById(R.id.et_Teljesitmeny);
+        btn_Szamol = (Button) findViewById(R.id.btn_Szamol);
+        btn_ElsoForgDatum = (Button) findViewById(R.id.btn_ElsoForgDatum);
+    }
+
+
+    // Első forgalombahelyezés datePicker nyitó
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "ElsoForg");
     }
 }
