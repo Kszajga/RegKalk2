d5545f91834444666a6f6e0b1eb498d73941d049
Kszajga <csygusz@gmail.com>
2018-03-18 13:08:14 +0100

    Next: Navigation Drawer

---
+++ app/src/main/java/regkalk2/kszajgapp/hu/regkalk/RegKalkKalkulacio.java
@@ -0,0 +1,28 @@
+package regkalk2.kszajgapp.hu.regkalk;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Csiga on 2018. 03. 18..
+ */
+
+public class RegKalkKalkulacio extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_reg_kalk_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Kalkuláció");
+    }
+}
